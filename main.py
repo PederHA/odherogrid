069d72e9e13c1b91c2ff25e2425e84496384d91e
PederHA <peder.andresen@gmail.com>
2019-08-27 14:47:08 +0200

    Too early for informative msgs

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys
+import json
 from enum import Enum
 from typing import List
 from pathlib import Path
 
 import requests
+import click
 
 from config import Config
-
+from resources import DOTA_GRID
 
 class Brackets(Enum):
     HERALD = 1
@@ -16,43 +18,35 @@ class Brackets(Enum):
     LEGEND = 5
     ANCIENT = 6
     DIVINE = 7
+    PRO = "pro"
 
 
-DOTA_GRID = {
-    "version": 3,
-    "configs": [
-        {
-            "config_name": "Opendota Hero Winrate",
-            "categories": [
-                {
-                    "category_name": "Heroes Sorted By Winrate",
-                    "x_position": 0.000000,
-                    "y_position": 0.000000,
-                    "width": 900.0,
-                    "height": 500.0,
-                    "hero_ids": [],
-                }
-            ],
-        }
-    ],
-}
+DEFAULT_BRACKET = Brackets.DIVINE.value
 
 
-def get_hero_stats(bracket: int) -> List[int]:
+def get_hero_stats() -> list:
     r = requests.get("https://api.opendota.com/api/heroStats")
-
     heroes = r.json()
+    return heroes
+
+
+def sort_heroes_by_winrate(heroes: list, bracket: str) -> list:
     heroes.sort(key=lambda h: h[f"{bracket}_pick"] / h[f"{bracket}_win"])
+    return heroes
 
-    return [hero["id"] for hero in heroes]
-
+def categorize_heroes_by_stat(heroes: list) -> dict:
+    heroes = {}
+    
 
 def format_hero_grid(heroes: List[int]) -> dict:
     d = DOTA_GRID.copy()
-    d["configs"][0]["categories"][0]["ids"].extend(heroes)
+    d["configs"][0]["categories"][0]["hero_ids"].extend(heroes)
+    return d
 
-def get_default_cfg_path():
-    d = f"Steam/userdata/{Config.USER_ID}/570/remote/cfg"
+
+def get_default_cfg_path() -> Path:
+    d = f"Steam/userdata/{Config.USER_ID}/570/remote/cfg" # choose random directory in userdata if no configured user?
+    
     if sys.platform == "win32":
         p = Path(f"C:/Program Files(x86)") / d
     elif sys.platform == "darwin":
@@ -60,17 +54,35 @@ def get_default_cfg_path():
     elif sys.platform == "linux":
         p = Path.home() / d
     else:
-        raise NotImplementedError("Your OS is not supported! Try to supply directory path manually.")
+        raise NotImplementedError("Hero grid directory auto detection is not supported for your OS!")
+    
+    return p
 
-if __name__ == "__main__":    
-    if len(sys.argv) > 1 and sys.argv[1].isdigit():
-        bracket = sys.argv[1]
-    else:
-        bracket = Config.DEFAULT_BRACKET
 
-    try:
-        cfg_path = sys.argv[2]
-    except IndexError:
+def update_hero_grid(path: Path, data: dict) -> None:
+    with open(path/"hero_grid_config.json", "w") as f:
+        json_data = json.dumps(data)
+        f.write(json_data)
+
+
+@click.command()
+@click.option("--bracket", "-b", default=DEFAULT_BRACKET)
+@click.option("--path", "-p", default=None)
+def main(bracket: str, path: str) -> None:
+    if not any(str(e.value) == bracket for e in Brackets):
+        bracket = DEFAULT_BRACKET
+    
+    if not path:
         cfg_path = get_default_cfg_path()
+    else:
+        cfg_path = Path(path)
+    
+    if not cfg_path.exists():
+        raise ValueError(f"User cfg directory '{cfg_path}' does not exist!")
+    
+    heroes = sort_heroes_by_winrate(get_hero_stats(), bracket=bracket)
 
-    heroes = get_hero_stats(bracket)
+    update_hero_grid(cfg_path, data)
+    
+if __name__ == "__main__":   
+    main()
---
+++ resources.py
@@ -0,0 +1,18 @@
+DOTA_GRID = {
+    "version": 3,
+    "configs": [
+        {
+            "config_name": "Opendota Hero Winrate",
+            "categories": [
+                {
+                    "category_name": "Heroes Sorted By Winrate",
+                    "x_position": 0.000000,
+                    "y_position": 0.000000,
+                    "width": 900.0,
+                    "height": 500.0,
+                    "hero_ids": [],
+                }
+            ],
+        }
+    ],
+}
