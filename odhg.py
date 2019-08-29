634157f963a7f188234944dea41f36ca414d65b0
PederHA <peder.andresen@gmail.com>
2019-08-29 09:56:07 +0200

    Change PRO value

--- odhg.py
+++ odhg.py
@@ -19,7 +19,7 @@ class Brackets(Enum):
     LEGEND = 5
     ANCIENT = 6
     DIVINE = 7
-    PRO = "pro"
+    PRO = 8
 
 
 DEFAULT_BRACKET = Brackets.DIVINE.value
@@ -28,7 +28,12 @@ DEFAULT_BRACKET = Brackets.DIVINE.value
 def get_hero_stats() -> list:
     """Retrieves hero win/loss statistics from OpenDotaAPI."""
     r = requests.get("https://api.opendota.com/api/heroStats")
-    return r.json()
+    heroes = r.json()
+    # Rename pro_<stat> to 8_<stat>, so it's easier to work with our enum
+    for hero in heroes:
+        for stat in ["win", "pick", "ban"]:
+            hero[f"{Brackets.PRO.value}_{stat}"] = hero.pop(f"pro_{stat}")
+    return heroes
 
 
 def sort_heroes_by_winrate(heroes: list, bracket: str) -> list:
