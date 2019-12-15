2eddbe859cd4b11c5db796c832c08102c5ee03fd
PederHA <peder.andresen@gmail.com>
2019-12-15 20:46:37 +0100

    Add _get_new_category() test

--- tests.py
+++ tests.py
@@ -205,6 +205,23 @@ def test_group_by_all():
     assert len(conf["categories"][0]["hero_ids"]) == N_HEROES
 
 
+def test__get_new_category():
+    arg = lambda: random.uniform(-3840, 3840)
+    kwargs = ["x_pos", "y_pos", "width", "height"]
+    for i in range(50):
+        assert _get_new_category("test", x_pos=arg())
+        assert _get_new_category("test", y_pos=arg())
+        assert _get_new_category("test", width=arg())
+        assert _get_new_category("test", height=arg())
+        
+        # Test random combination (and number) of keywords
+        random.shuffle(kwargs)
+        kw = kwargs[0:random.randint(1, len(kwargs))]
+        params = {k: arg() for k in kw}
+        assert _get_new_category("test", **params)
+
+
+
 # config.py
 def test__load_config():
     """Tests `config._load_config()`"""
