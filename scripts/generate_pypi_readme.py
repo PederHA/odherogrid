87a2efb93ca3991b38995bb1efc5437b3440df91
PederHA <peder.andresen@gmail.com>
2020-01-25 18:06:51 +0100

    Structure

--- scripts/generate_pypi_readme.py
+++ scripts/generate_pypi_readme.py
@@ -1,4 +1,4 @@
-if __name__ == "__main__":
+def make_pypi_readme() -> None:
     long_description = ""
     with open("README.md", "r") as f:
         long_description += f.read()
@@ -6,3 +6,7 @@ if __name__ == "__main__":
         long_description += f.read()
     with open("PYPIREADME.md", "w") as f:
         f.write(long_description)
+
+
+if __name__ == "__main__":
+    make_pypi_readme()
