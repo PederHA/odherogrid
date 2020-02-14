5c3bc5b2cdb8ec931ba2e7f4e984aa3f0dc371ea
PederHA <peder.andresen@gmail.com>
2020-02-14 18:04:14 +0100

    Add dist tests

---
+++ tests/test_dist.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+import toml
+
+from odherogrid import __version__
+
+
+ODHG_ROOT = Path(__file__).parent.parent # same as var in scripts/resouces.py
+with open(ODHG_ROOT/"pyproject.toml") as f:
+    pyproject = toml.load(f)
+
+
+def test_version():
+    assert __version__ == pyproject["tool"]["poetry"]["version"] == "0.2.0"
